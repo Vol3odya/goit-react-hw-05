7aac63d2602cd34deebe15f4ea7f38884b10b7ba
Vol3odya <Vol_odya@ukr.net>
2024-09-29 08:41:46 +0300

    finish3

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation, useParams } from "react-router-dom"
 import {castMovie} from "../../http/http-movies"
 import css from "./MovieCast.module.css"
 
 export default function MovieCast() {
   const location = useLocation();
-  const id = parseInt(location.pathname.substr(8, 14));
+  const { movieId } = useParams();
   const [cast, setCast] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -16,7 +16,7 @@ export default function MovieCast() {
             try {
                 setLoading(true);
                 setError(false);
-                const data = await castMovie(id);
+                const data = await castMovie(movieId);
                 setCast(data.data.cast);
                 
             } catch (error) {
@@ -26,7 +26,7 @@ export default function MovieCast() {
             }
         }
         fetchData();
-    }, []);
+    }, [movieId]);
 
   return (
     <ul>
--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation, useParams } from "react-router-dom"
 import {reviewMovie} from "../../http/http-movies"
 import css from "./MovieReviews.module.css"
 
 export default function MovieReviews() {
  const location = useLocation();
-  const id = parseInt(location.pathname.substr(8, 14));
+  const { movieId } = useParams();
   const [review, setReview] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -15,7 +15,7 @@ export default function MovieReviews() {
             try {
                 setLoading(true);
                 setError(false);
-              const data = await reviewMovie(id);
+              const data = await reviewMovie(movieId);
                 setReview(data.data.results);
                 
             } catch (error) {
@@ -25,7 +25,7 @@ export default function MovieReviews() {
             }
         }
         fetchData();
-    }, []);
+    }, [movieId]);
 
   return (
     <ul>
